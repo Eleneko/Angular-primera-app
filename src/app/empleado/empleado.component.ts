f93e1707c887567905a2058a7cebeedf7066bc74
Eleneko <miraisoncloudi@gmail.com>
2023-03-11 02:48:05 -0300

    Componente inline

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -3,7 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-empleado',
   templateUrl: './empleado.component.html',
+  //
+  //HTML INLINE
+  // template:"<p>Aqui va un empleado</p>",
+  //
   styleUrls: ['./empleado.component.css']
+  //Se puede omitir la direccion del css para hacerlo directamente (inline) en el caso de una app sencilla
+  //usar styles? en vez de styleUrls?
+  //CSS INLINE
+  // styles:['p{background-color:pink;}']
+  //
+  //
 })
 export class EmpleadoComponent {
 
